fafdcb4d70f2cc6a5296acfa8ed61c171b03e8d4
Nasakareal <naasaka@outlook.es>
2026-01-17 17:05:22 -0600

    not null

--- database/migrations/2026_01_13_045709_alter_vehiculos_make_serie_and_tarjeta_nullable.php
+++ database/migrations/2026_01_13_045709_alter_vehiculos_make_serie_and_tarjeta_nullable.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -16,6 +17,14 @@ return new class extends Migration
 
     public function down()
     {
+        DB::table('vehiculos')
+            ->whereNull('serie')
+            ->update(['serie' => '']);
+
+        DB::table('vehiculos')
+            ->whereNull('tarjeta_circulacion_nombre')
+            ->update(['tarjeta_circulacion_nombre' => '']);
+
         Schema::table('vehiculos', function (Blueprint $table) {
             $table->string('serie', 17)->nullable(false)->change();
             $table->string('tarjeta_circulacion_nombre', 60)->nullable(false)->change();

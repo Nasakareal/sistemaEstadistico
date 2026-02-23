aa6a08582035c682adfda0e477ad0cd789082794
Nasakareal <naasaka@outlook.es>
2026-02-23 17:42:16 -0600

    kilometraje

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/home', [DashboardController::class, 'home'])->name('api.home');
-    Route::get('/home/perito', [DashboardController::class, 'homePerito'])->middleware('can:ver home perito')->name('api.home.perito');
 
 
     Route::post('/device-tokens', [DeviceTokenController::class, 'store']);

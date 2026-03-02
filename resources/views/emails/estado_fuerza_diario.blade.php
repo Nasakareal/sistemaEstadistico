50f6e7009abffb68864ee0a41ca54a7b3f2ebfee
Nasakareal <naasaka@outlook.es>
2026-03-01 19:10:49 -0600

    fecha pendeja

--- resources/views/emails/estado_fuerza_diario.blade.php
+++ resources/views/emails/estado_fuerza_diario.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
 </head>
 <body>
-    <p>Se adjunta el Estado de Fuerza correspondiente al corte: <strong>{{ $fechaTexto }}</strong>.</p>
+    <p>Se adjunta el Estado de Fuerza correspondiente al corte: <strong>{{ $fecha }}</strong>.</p>
     <p>— Sistema Estadístico</p>
 </body>
 </html>

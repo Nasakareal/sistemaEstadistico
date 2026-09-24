56023db0f5d94419e3524da1ed6f19ce14d069c6
Nasakareal <naasaka@outlook.es>
2026-09-24 17:31:31 -0600

    cambio de carto

--- resources/views/waze/riesgo/index.blade.php
+++ resources/views/waze/riesgo/index.blade.php
@@ -924,9 +924,9 @@
         preferCanvas: true
     }).setView(centerDefault, 12);
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        maxZoom: 20,
-        attribution: '&copy; OpenStreetMap &copy; CARTO'
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     const layerHechos = L.layerGroup();
